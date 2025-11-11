4c2dfe2966d1bc89549a38c5929816673aef87cc
Таисия <mortiesss@mail.ru>
2025-11-11 13:58:48 +0300

    Update analyser1.sh

--- analyser1.sh
+++ analyser1.sh
@@ -2,6 +2,11 @@
 
 for i in $(ls $1) 
 do
-  for i in $(cat $1$i)
+  for j in $(cat $1$i)
   do
-    
+    name=$(echo $j | cut -d ":" -f1)
+    age=$(echo $j | cut -d ":" -f2)
+    email=$(echo $j | cut -d ":" -f3)
+    echo "Name: $name | Age: $age | Email: $email"
+  done
+done
